625624ee7bd4cb47b9de01da62c7d5e31372d9cd
Nairit-Sinha-VI-B <nairitsinha@gmail.com>
2026-04-01 21:12:12 +0530

    Add files via upload

---
+++ (4)(HW)Activity 1(Square Root Calculation of Any Number).py
@@ -0,0 +1,9 @@
+# Ask for number
+
+from math import sqrt
+Number = int(input("Enter a Random number as per your choice:"))
+# Calculate Square Root
+
+Sqrt = sqrt(Number)
+
+print("The square root of the number is:", Sqrt)

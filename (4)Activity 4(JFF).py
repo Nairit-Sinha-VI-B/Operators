63e419fd0c3834e739bb1c33476e0e93590d29a6
Nairit-Sinha-VI-B <nairitsinha@gmail.com>
2026-04-01 21:06:58 +0530

    Add files via upload

---
+++ (4)Activity 4(JFF).py
@@ -0,0 +1,20 @@
+# Take input
+
+N1 = int(input("Enter the first number: "))
+N2 = int(input("Enter the second number: "))
+
+# Perform arithmetic operations
+
+Sum = N1 + N2
+Difference = N1 - N2
+Product = N1 * N2
+Quotient = N1 / N2
+Reimainder = N1 % N2
+
+# Display the results
+
+print("The sum of the two numbers is:", Sum)
+print("The difference of the two numbers is:", Difference)
+print("The product of the two numbers is:", Product)
+print("The quotient of the two numbers is:", Quotient)
+print("The remainder of the two numbers is:", Reimainder)
